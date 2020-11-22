98ee70dbc62f22f589334eadc853e80dcfd1ec89
elizarpif <liza.pif@mail.ru>
2020-11-22 14:21:02 +0300

    added grpc gateway

--- api/gen.go
+++ api/gen.go
@@ -1,2 +1,2 @@
-//go:generate protoc -I =. --go_out=plugins=grpc:./ --grpc-gateway_out=logtostderr=true:./ torrent.proto
+//go:generate protoc -I =. --go_out=plugins=grpc:./ --swagger_out=logtostderr=true:./ --grpc-gateway_out=logtostderr=true:./ torrent.proto
 package api
--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 )
