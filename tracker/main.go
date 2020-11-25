6fef1f8a497e3bc2f80b115f1119b0c22970fdde
elizarpif <liza.pif@mail.ru>
2020-11-25 23:39:17 +0300

    changed logger

--- tracker/main.go
+++ tracker/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"net"
 	"net/http"
-	"os"
 
 	"github.com/elizarpif/grpctorrent/api"
+	"github.com/elizarpif/logger"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/sirupsen/logrus"
 
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
@@ -19,37 +17,12 @@ const (
 	httpAddr    = "localhost:8000"
 )
 
-func setContext() context.Context {
-	return context.WithValue(context.Background(), "logger", newLogger())
-}
-
-func newLogger() *logrus.Logger {
-	log := logrus.StandardLogger()
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(logrus.DebugLevel)
-
-	return log
-}
-
-//noinspection GoUnresolvedReference
-func getLogger(ctx context.Context) *logrus.Logger {
-	value := ctx.Value("logger")
-	log, ok := value.(*logrus.Logger)
-	if !ok {
-		log = newLogger()
-	}
-
-	return log
-}
-
 func main() {
-	logger := newLogger()
+	log := logger.NewLogger()
 
 	lis, err := net.Listen("tcp", grpcAddress)
 	if err != nil {
-		logger.WithError(err).WithField("address", grpcAddress).Fatal("listen for grpc")
+		log.WithError(err).WithField("address", grpcAddress).Fatal("listen for grpc")
 	}
 
 	grpcServer := grpc.NewServer()
@@ -58,12 +31,12 @@ func main() {
 	server := NewServer()
 	api.RegisterTrackerServer(grpcServer, server)
 
-	ctx := setContext()
+	ctx := logger.SetContext(log)
 
 	mux := runtime.NewServeMux()
 	err = api.RegisterTrackerHandlerFromEndpoint(ctx, mux, grpcAddress, []grpc.DialOption{grpc.WithInsecure()})
 	if err != nil {
-		logger.WithError(err).Fatal("cannot register")
+		log.WithError(err).Fatal("cannot register")
 	}
 
 	srv := &http.Server{
@@ -73,17 +46,17 @@ func main() {
 
 	group := errgroup.Group{}
 	group.Go(func() error {
-		logger.WithField("address", grpcAddress).Info("start grpc server")
+		log.WithField("address", grpcAddress).Info("start grpc server")
 		return grpcServer.Serve(lis)
 	})
 
 	group.Go(func() error {
-		logger.WithField("address", httpAddr).Info("start http server")
+		log.WithField("address", httpAddr).Info("start http server")
 		return srv.ListenAndServe()
 	})
 
 	err = group.Wait()
 	if err != nil {
-		logger.WithError(err).Fatal("group wait")
+		log.WithError(err).Fatal("group wait")
 	}
 }
